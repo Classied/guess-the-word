f9f25c74d31a07c036cd6a71e5e4aa194e89b5b6
classie_d <c.monee.davidson@gmail.com>
2023-01-06 16:39:54 -0600

    Added functions to display word and guessed letters

--- js/script.js
+++ js/script.js
@@ -66,5 +66,42 @@ const makeGuess = function (guess) {
     } else {
         guessedLetters.push(guess);
         console.log(guessedLetters);
+        showGuessedLetters();
+        updateWordInProgress(guessedLetters);
+    }
+};
+
+const showGuessedLetters = function () {
+    guessedLettersElement.innerHTML = "";
+    for (const letter of guessedLetters) {
+        const li = document.createElement("li");
+        li.innerText = letter;
+        guessedLettersElement.append(li);
+    }
+};
+
+const updateWordInProgress = function (guessedLetters) {
+    const wordUpper = word.toUpperCase();
+
+    const wordArray = wordUpper.split("");
+
+    const updatedCharacters = [];
+
+    for (const letter of wordArray) {
+        if (guessedLetters.includes(letter)) {
+            updatedCharacters.push(letter.toUpperCase());
+        } else {
+            updatedCharacters.push("●");
+        }
+    }
+    wordInProgress.innerText = updatedCharacters.join("");
+    playerWins();
+
+};
+
+const playerWins = function () {
+    if (word.toUpperCase() === wordInProgress.innerText) {
+        message.classList.add("win");
+        message.innerHTML = `<p class="highlight">You Guessed the correct word! Congrats!</p>`;
     }
 };
